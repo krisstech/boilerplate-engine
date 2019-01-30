3150b05a11db007762982af64556c7a230ad45ed
Kristaps Dzērve-Štrāls <kristaps.dzerve@visma.com>
2019-01-30 15:31:59 +0200

    simple command

---
+++ server/BoilerplateEngine.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace BoilerplateEngine.Console
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using BoilerplateEngine.Core;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cmd = "echo 'test'";
+
+            string result;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                result = cmd.ExecuteCmd();
+            }
+            else {
+
+                result = cmd.ExecuteBash();
+            }
+            
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
