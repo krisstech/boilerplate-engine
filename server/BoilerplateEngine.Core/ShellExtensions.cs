c828a8d54fe5eeb6cf281f43a5b0d10fc4fecd3a
Kristaps Dzērve-Štrāls <kristaps.dzerve@visma.com>
2019-01-30 15:32:28 +0200

    server exec test

---
+++ server/BoilerplateEngine.Core/ShellExtensions.cs
@@ -0,0 +1,50 @@
+namespace BoilerplateEngine.Core
+{
+    using System;
+    using System.Diagnostics;
+
+    public static class ShellExtensions
+    {
+        public static string ExecuteBash(this string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+            
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+
+        public static string ExecuteCmd(this string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+            
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = $"/C \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+    }
+}
