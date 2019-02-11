72a9935691f12c0da642ca360cd9153cbd4879d5
Kristaps Dzērve-Štrāls <kristaps.dzerve@visma.com>
2019-02-11 09:14:17 +0200

    fix form state

--- client/src/components/Server/Dotnet/WebApi/EfCoreForm.tsx
+++ client/src/components/Server/Dotnet/WebApi/EfCoreForm.tsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
 
 interface IFormProps {
     handleFormChange: (event: React.ChangeEvent<HTMLSelectElement>) => void
 }
 
-const EfCoreForm: React.SFC<IFormProps> = (props: IFormProps) => {
+interface IState {
+    value: string;
+}
+class EfCoreForm extends Component<IFormProps, IState> {
 
-    return (
-        <FormControl style={{minWidth: '200px'}}>
-            <InputLabel htmlFor="efcore">Use EF Core?</InputLabel>
-            <Select
-                onChange={props.handleFormChange}
-                inputProps={{
-                name: 'efcore',
-                id: 'efcore',
-                }}
-            >
-                <MenuItem key={1} value="true">Yes</MenuItem>
-                <MenuItem key={2} value="false">No</MenuItem>
-            </Select>
-        </FormControl>
-    );
+    constructor(props: IFormProps) {
+        super(props);
+        this.state = {
+            value: 'true'
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        this.setState({ value: event.target.value })
+
+        this.props.handleFormChange(event);
+    }
+
+    render() {
+        return (
+            <FormControl style={{ minWidth: '200px' }}>
+                <InputLabel htmlFor="efcore">Use EF Core?</InputLabel>
+                <Select
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                    inputProps={{
+                        name: 'efcore',
+                        id: 'efcore',
+                    }}
+                >
+                    <MenuItem key={1} value="true">Yes</MenuItem>
+                    <MenuItem key={2} value="false">No</MenuItem>
+                </Select>
+            </FormControl>
+        );
+    }
 }
 
 export default EfCoreForm;
--- client/src/components/Server/Dotnet/WebApi/SwaggerForm.tsx
+++ client/src/components/Server/Dotnet/WebApi/SwaggerForm.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
 
 interface IFormProps {
     handleFormChange: (event: React.ChangeEvent<HTMLSelectElement>) => void
 }
 
-const SwaggerForm: React.SFC<IFormProps> = (props: IFormProps) => {
-  return (
-    <FormControl style={{minWidth: '200px'}}>
-        <InputLabel htmlFor="swagger">Use Swagger UI?</InputLabel>
-        <Select
-            onChange={props.handleFormChange}
-            inputProps={{
-            name: 'swagger',
-            id: 'swagger',
-            }}
-        >
-            <MenuItem key={1} value="true">Yes</MenuItem>
-            <MenuItem key={2} value="false">No</MenuItem>
-        </Select>
-    </FormControl>
-);
+interface IState {
+    value: string;
+}
+
+class SwaggerForm extends Component<IFormProps, IState> {
+
+    constructor(props: IFormProps) {
+        super(props);
+        this.state = {
+            value: 'true'
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        this.setState({ value: event.target.value })
+
+        this.props.handleFormChange(event);
+    }
+
+    render() {
+        return (
+            <FormControl style={{ minWidth: '200px' }}>
+                <InputLabel htmlFor="swagger">Use Swagger UI?</InputLabel>
+                <Select
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                    inputProps={{
+                        name: 'swagger',
+                        id: 'swagger',
+                    }}
+                >
+                    <MenuItem key={1} value="true">Yes</MenuItem>
+                    <MenuItem key={2} value="false">No</MenuItem>
+                </Select>
+            </FormControl>
+        );
+    }
 }
 
 export default SwaggerForm;
